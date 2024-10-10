8570f596e8bbdabfa95f823961291ba0a2267130
Ye'ela Granot <yeela8g@gmail.com>
2024-10-10 17:51:28 +0300

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const {c_login_user, c_register_user, c_isSurvey, c_update_Complete_Tasks, c_is_complete_tasks} = require('./controller.js');
+const {c_login_user, c_register_user, c_isSurvey, c_update_Complete_Tasks, c_is_complete_tasks, status} = require('./controller.js');
 
 router.route('/login').post(c_login_user);
 router.route('/register').post(c_register_user);
 router.route('/tasks/:userID').get(c_isSurvey);
 router.route(`/completeTasks/:userID`).put(c_update_Complete_Tasks);
 router.route(`/completeTasks/:userID`).get(c_is_complete_tasks);
-router.route('/status', (req, res) => {
-  console.log("status is sending 200");
-  res.status(200).json({ status: 'ok' });
-});
+router.route('/status').get(status);
+
 
 
 module.exports = router;
