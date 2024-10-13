46f9c7f600f64c41d91e72902d3e6cc0974083db
Ye'ela Granot <yeela8g@gmail.com>
2024-10-13 18:25:01 +0300

    Update AdminDashboard.js

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -4,6 +4,7 @@ function AdminDashboard({ userID, handleLogout }) {
   const [newUserID, setNewUserID] = useState("");
   const [newUserPhoneNumber, setNewUserPhoneNumber] = useState(""); //not really phone number
   const [newUserStartDate, setNewUserStartDate] = useState("");
+  const [protocolType, setProtocolType] = useState("V1");
 
   const handleAddUser = async (e) => {
       e.preventDefault();
@@ -13,13 +14,16 @@ function AdminDashboard({ userID, handleLogout }) {
         body: JSON.stringify({
           userID: newUserID,
           phoneNumber: newUserPhoneNumber,
-          startDate: newUserStartDate
+          startDate: newUserStartDate,
+          protocolType: protocolType
         })
       });
       if (response.status === 200) {
         alert("המשתמש נוסף בהצלחה!");
         setNewUserID("");
         setNewUserPhoneNumber("");
+        setProtocolType("V1");
+
       } else {
         alert("הוספה נכשלה או קלט לא תקין");
       }
@@ -28,15 +32,23 @@ function AdminDashboard({ userID, handleLogout }) {
   return (
     <div className="relative flex flex-col justify-center items-center min-h-screen p-4 sm:p-6 lg:p-8 bg-gradient-to-r from-gray-950 to-blue-600">
       <div className="absolute top-8">
-        <img src="/logo.webp" alt="logo" width={100} height="auto" className="md:w-32" />
+        <img
+          src="/logo.webp"
+          alt="logo"
+          width={80}
+          height="auto"
+          className="md:w-32"
+        />
       </div>
-  
+
       <div className="bg-gray-300 p-6 sm:p-8 rounded border border-gray-400 shadow-md w-full max-w-md mt-16 md:mt-24 lg:mt-32">
         <h1 className="text-xl sm:text-2xl lg:text-3xl font-extrabold mb-4 text-blue-900 text-center">
           {`! ${userID} ברוך הבא`}
         </h1>
         <h2 className="text-lg font-bold mb-2 text-blue-900 text-center">
-          <span className="border-b border-gray-50 border-dashed">: הוספת נסיין</span>
+          <span className="border-b border-gray-50 border-dashed">
+            : הוספת נסיין
+          </span>
         </h2>
         <form onSubmit={handleAddUser}>
           <input
@@ -55,27 +67,33 @@ function AdminDashboard({ userID, handleLogout }) {
             onChange={(e) => setNewUserPhoneNumber(e.target.value)}
             required
           />
-         <div className="relative mb-4">
-  <label 
-    htmlFor="startDate"
-    className="block text-gray-700 text-right mb-1"
-  >
-              :V1 תאריך התחלה
-  </label>
-  <input
-    id="startDate"
-    type="date"
-    className="shadow border rounded w-full py-2 px-3 text-gray-700"
-    value={newUserStartDate}
-    onChange={(e) => setNewUserStartDate(e.target.value)}
-    required
-  />
-</div>
 
-          <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full mb-4">
+            <input
+              id="startDate"
+              type="date"
+              className="shadow border rounded w-full py-2 px-3 mb-1 text-gray-700"
+              value={newUserStartDate}
+              onChange={(e) => setNewUserStartDate(e.target.value)}
+              required
+            />
+
+            <select
+              className="mt-1 shadow border rounded w-full py-2 px-3 mb-4 text-right text-gray-700"
+              value={protocolType}
+              onChange={(e) => setProtocolType(e.target.value)}
+              required
+            >
+              <option value="v1">V1</option>
+              <option value="safety">Safety</option>
+            </select>
+
+          <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full mb-2">
             הוספה
           </button>
-          <button onClick={handleLogout} className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded w-full">
+          <button
+            onClick={handleLogout}
+            className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded w-full"
+          >
             סיום
           </button>
         </form>
